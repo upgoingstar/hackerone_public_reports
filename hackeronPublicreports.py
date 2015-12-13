5543c589b9e96993dc268cb8854c1e297fbc8296
Shubham Mittal <shubham.mittal@olacabs.com>
2015-12-13 14:35:54 +0530

    Changed Logic, Writing to CSV

--- hackeronPublicreports.py
+++ hackeronPublicreports.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import csv
 dictbugs = {}
+
 
 
 
@@ -14,26 +16,21 @@ def find_latest_id(url):
 		for tag in soup.find_all('a'):
 			if "report" in tag.get('href'):
 				reportid = tag.get('href').split("/")[2]
-				dictbugs[reportid] = tag.text
+				reportjson = "https://hackerone.com/reports/" + reportid + ".json"
+				req1 = requests.get(reportjson)
+				data = json.loads(req1.content)
+				print data['url']
+				if 'formatted_bounty' in data.keys():
+					cellwriter.writerow([data['id'], data['url'], data['title'].encode('utf-8').strip(), data['formatted_bounty'], data['reporter']['username'].encode('utf-8').strip(),data['team']['handle'].encode('utf-8').strip(),data['created_at'], data['disclosed_at']])
+				else:
+					cellwriter.writerow([data['id'], data['url'], data['title'].encode('utf-8').strip(), "Bounty Info Not sure", "No Reporter Information FOund",data['team']['handle'].encode('utf-8').strip(),data['created_at'], data['disclosed_at']])
 			else:
 				pass
 	except:
 		pass
 
+cellwriter = csv.writer(open('eggs.csv', 'w'))
 for x in xrange(1,171):
 	url = "https://hackerone.com/hacktivity?page=" + str(x)
 	find_latest_id(url)
-	print "Checking on" + url
-
-#print dictbugs
-
-f = open("reports.csv", 'w')
-for key in dictbugs:
-	text = "https://hackerone.com/reports/" +key + "," + dictbugs[key] + "\n"
-	f.write(text)
-
-
-		
-
-#for x in xrange(1,):
-#	req = requests.get("https://hackerone.com/reports/104028")
+	print "Checking on " + url
